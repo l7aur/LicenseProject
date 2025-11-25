b1071a148f94d854df09f367fff3c54b851a4a7b
l7aur <l7aur@L7AUR-WINDOWS>
2025-11-25 11:10:19 +0200

    doxygen template

--- LicenseProject/Point2.hpp
+++ LicenseProject/Point2.hpp
@@ -1,15 +1,52 @@
 #pragma once
 
+/**
+ * .
+ */
 class Point2 {
 public:
+	/**
+	 * .
+	 * 
+	 * \param _x
+	 * \param _y
+	 */
 	Point2(const double _x, const double _y);
+	
+	/**
+	 * .
+	 * 
+	 */
 	Point2();
+	
+	/**
+	 * .
+	 * 
+	 */
 	~Point2() = default;
 
+	/**
+	 * .
+	 * 
+	 * \return 
+	 */
 	[[nodiscard]] double getX() const { return x; }
+	
+	/**
+	 * .
+	 * 
+	 * \return 
+	 */
 	[[nodiscard]] double getY() const { return y; }
 
 private:
+	/**
+	 * .
+	 */
 	double x;
+	
+	/**
+	 * .
+	 */
 	double y;
 };
